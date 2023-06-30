fe7f6932bcaf10e7b7199e0c60be5a643bb53908
Mateeus10 <mateeus.almeida10@gmail.com>
2023-06-30 15:39:32 -0300

    Mudando as portas

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _path = require('path');
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
-
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 require('./database');
 
@@ -10,6 +11,22 @@ var _produtoRoutesjs = require('./routes/produtoRoutes.js'); var _produtoRoutesj
 var _categoryRoutesjs = require('./routes/categoryRoutes.js'); var _categoryRoutesjs2 = _interopRequireDefault(_categoryRoutesjs);
 var _fotoRoutesjs = require('./routes/fotoRoutes.js'); var _fotoRoutesjs2 = _interopRequireDefault(_fotoRoutesjs);
 var _tokenRoutesjs = require('./routes/tokenRoutes.js'); var _tokenRoutesjs2 = _interopRequireDefault(_tokenRoutesjs);
+
+const whiteList = [
+  'http://localhost:3000',
+
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
 
 
 
@@ -21,6 +38,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import express from 'express';
-
+import cors from 'cors';
+import helmet from 'helmet';
 
 import './database';
 
@@ -10,6 +11,22 @@ import productRoutes from './routes/produtoRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import fotoRoutes from './routes/fotoRoutes.js';
 import tokenRoutes from './routes/tokenRoutes.js';
+
+const whiteList = [
+  'http://localhost:3000',
+
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
 
 
 
@@ -21,6 +38,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
